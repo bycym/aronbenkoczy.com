bb207efb915fa737d7f78b4ed8a9256271d5e697
Aron <aronbenkoczy@kinsta.com>
2025-01-04 16:34:23 +0100

    feat: config update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-
-import tailwind from '@astrojs/tailwind';
-
+import react from '@astrojs/react'
+import tailwind from '@astrojs/tailwind'
+import { defineConfig } from 'astro/config'
 // https://astro.build/config
 export default defineConfig({
-    // Enable React to support React JSX components.
-    integrations: [react(), tailwind()],
-});
+  // Enable React to support React JSX components.
+  integrations: [react(), tailwind()]
+})
