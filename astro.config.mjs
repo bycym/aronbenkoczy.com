790171c8615f4c2846304f52fee2ef7dbf1419b0
Aron <aronbenkoczy@kinsta.com>
2025-01-08 08:45:57 +0100

    feat: deploy astro to github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import { defineConfig } from 'astro/config'
 // https://astro.build/config
 export default defineConfig({
   // Enable React to support React JSX components.
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind()],
+  site: 'https://bycym.github.io',
+  base: 'aronbenkoczy.com'
 })
